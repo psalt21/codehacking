9f67d89ccfd19c1bdd809f32bb041a65e0fed547
Phil Salter <phil@goreact.com>
2019-07-11 14:35:23 -0600

    update services to use Braintree config info

--- config/services.php
+++ config/services.php
@@ -40,4 +40,11 @@ return [
         ],
     ],
 
+    'braintree' => [
+        'environment' => env('BT_ENVIRONMENT', 'sandbox'),
+        'merchantId' => env('BT_MERCHANT_ID'),
+        'publicKey' => env('BT_PUBLIC_KEY'),
+        'privateKey' => env('BT_PRIVATE_KEY'),
+    ],
+
 ];
